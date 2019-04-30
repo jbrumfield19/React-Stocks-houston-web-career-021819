16338d1b6e8a24cc1278f6953424edbe11fe9526
Jessica Brumfield <jessicabrumfield@Jessicas-MacBook-Pro.local>
2019-04-30 17:21:53 -0500

    Done.

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -4,8 +4,30 @@ import PortfolioContainer from './PortfolioContainer'
 import SearchBar from '../components/SearchBar'
 
 class MainContainer extends Component {
+  state={stocks:[]}
+  grabStocks(){
+    fetch('http://localhost:3000/stocks')
+    .then(res=>res.json())
+    .then((stecks)=>{this.setState({stocks:stecks})})
+  }
+  handleClick=(id)=>{
+    this.setState({
+      stocks:this.state.stocks.map(stock=>{
+      if(stock.id=== id ){
+        return {...stock, owned:!stock.owned}
+      }
+      else{
+        return stock
+      }
+    })
 
+      
+    })
+
+  }
   render() {
+   const portStock = this.state.stocks.filter(stock=> stock.owned===true)
+   const stockStock = this.state.stocks.filter(stock=> stock.owned===false)
     return (
       <div>
         <SearchBar/>
@@ -13,19 +35,21 @@ class MainContainer extends Component {
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+              <StockContainer stocks={stockStock} handleClick={this.handleClick}/>
 
             </div>
             <div className="col-4">
 
-              <PortfolioContainer/>
+              <PortfolioContainer stocks={portStock} handleClick={this.handleClick}/>
 
             </div>
           </div>
       </div>
     );
   }
-
+  componentDidMount(){
+    this.grabStocks()
+  }
 }
 
 export default MainContainer;
--- src/containers/StockContainer.js
+++ src/containers/StockContainer.js
@@ -2,18 +2,18 @@ import React, { Component } from 'react';
 import Stock from '../components/Stock'
 
 class StockContainer extends Component {
-
+ 
   render() {
     return (
       <div>
         <h2>Stocks</h2>
-        {
+        {this.props.stocks.map((stock)=>{return <Stock {...stock} handleClick={this.props.handleClick}/>})
           //render the list of stocks here
         }
       </div>
     );
   }
-
+  
 }
 
 export default StockContainer;
